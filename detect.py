502eefd3e135e33ff38ea42b5befbbe0f1539242
Ringkun <ringkun12@gmail.com>
2025-06-10 12:28:14 -0600

    Moved all model files to a new folder. Might use improved models in the future. Figure out a quick switch.

--- detect.py
+++ detect.py
@@ -46,13 +46,17 @@ parser=argparse.ArgumentParser()
 parser.add_argument('--image')
 parser.add_argument('--frame')
 args=parser.parse_args()
-# List of neural networks
-faceProto="opencv_face_detector.pbtxt"
-faceModel="opencv_face_detector_uint8.pb"
-ageProto="age_deploy.prototxt"
-ageModel="age_net.caffemodel"
-genderProto="gender_deploy.prototxt"
-genderModel="gender_net.caffemodel"
+
+MODELROOTFOLDER = "Models/"
+
+# List of Models
+faceProto= MODELROOTFOLDER+"opencv_face_detector.pbtxt"
+faceModel= MODELROOTFOLDER+"opencv_face_detector_uint8.pb"
+ageProto= MODELROOTFOLDER+"age_deploy.prototxt"
+ageModel= MODELROOTFOLDER+"age_net.caffemodel"
+genderProto= MODELROOTFOLDER+"gender_deploy.prototxt"
+genderModel= MODELROOTFOLDER+"gender_net.caffemodel"
+
 # No clue on where the model mean values were calculated from.
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
 # Partitian of age and gender
@@ -109,21 +113,17 @@ while True:
         for faceBox in faceBoxes:
             face=frame[max(0,faceBox[1]-padding):min(faceBox[3]+padding,frame.shape[0]-1),
                         max(0,faceBox[0]-padding):min(faceBox[2]+padding, frame.shape[1]-1)]
-
             # # These uses neural networks to regress the gender of the facs detected in the image.
             blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
-
             # genderNet.setInput(blob)
             # genderPreds=genderNet.forward()
             # gender=genderList[genderPreds[0].argmax()]
-
             # These uses neural networks to regress the age of the facs detected in the image.
             ageNet.setInput(blob)
             agePreds=ageNet.forward()
             age=ageList[agePreds[0].argmax()]
             # cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)    
             cv2.putText(resultImg, f'{age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)    
-
     # Render Image
     cv2.imshow(windowName, resultImg)
 
